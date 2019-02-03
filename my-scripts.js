025515d8fe9a40c0cf39666bd87430f4d22831f4
kurofuneparry <kurofuneparry@gmail.com>
2019-02-02 18:20:41 -0800

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,29 @@
+function Item(x) {
+	this.text = x;
+	this.state = "none";
+	this.clicked = function(){
+		this.state = (this.state == "none") ? "strike" : "invisible";
+	}
+}
+
+items = (JSON.parse(localStorage.getItem("myData")) || []).map(i => Item(...i))
+
+function printItem(item) {
+	div = document.createElement("p");
+	div.innerHTML = item.text;
+	div.classList.add(item.state);
+	div.addEventListener("click", function (element) {
+		item.clicked()
+		localStorage.setItem("items", JSON.stringify(items));
+		element.target.classList.add(item.state);
+	})
+	document.getElementById("list").appendChild(div);
+}
+for (let i=0; i < items.length; i++) {printItem(items[i]);}
+ 
+function createItem() {
+	newItem = new Item(prompt("Your new item prompt goes here"));
+	items.push(newItem);
+	localStorage.setItem("items", JSON.stringify(items));
+	printItem(newItem);
+}
